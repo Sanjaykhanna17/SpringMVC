f7fe559125e7fe74cd72ee4b2a14f5dc87233ea5
Sanjaykhanna <sanjaykhanna9122001@gmail.com>
2024-05-07 20:41:51 +0530

    first commit

---
+++ src/test/java/com/journaldev/spring/TestClass.java
@@ -0,0 +1,14 @@
+package com.journaldev.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void test() {
+		assertTrue(!false);
+	}
+
+}
